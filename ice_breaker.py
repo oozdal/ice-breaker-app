fa7d7bbde82b3a1bf8e2f8feb237c2fe1020cc32
Ozer, Ozdal <ozerozdal@gmail.com>
2024-04-15 16:00:24 -0400

    minor update ice_breaker.py

--- ice_breaker.py
+++ ice_breaker.py
@@ -26,24 +26,25 @@ if __name__ == "__main__":
 
     information = st.text_area("Provide some information about the person you would like to search", default_info)
 
-    linkedin_profile_url = linkedin_lookup_agent(name=name)
-
-    summary_template = """
-    given the information {information} about a person I want you to create:
-    1. A short summary
-    2. two interesting facts about them
-    """
-
     if st.button("Submit"):
         try:
 
             if name == default_name:
-                # Get information from saved json file
+                # If the user searches "Ozer Ozdal", do not send API requests.
+                # Fetch the information from the results/summary.json file as a cost-conscious application
                 with open("results/summary.json") as f:
                     summary = json.load(f)
                     st.success(summary['text'])
 
             else:
+                linkedin_profile_url = linkedin_lookup_agent(name=name)
+
+                summary_template = """
+                given the information {information} about a person I want you to create:
+                1. A short summary
+                2. two interesting facts about them
+                """
+
                 summary_prompt_template = PromptTemplate(
                     input_variables=["information"], template=summary_template
                 )
@@ -60,15 +61,8 @@ if __name__ == "__main__":
 
                 summary = chain.invoke(input={"information": linkedin_data})
 
-                #print(summary['information'])
-                #print(summary['text'])
-
-                # Return the short summary and two interesting facts about the profile
+                # Return the short summary and two interesting facts about the person
                 st.success(summary['text'])
-
-                # Save the summary
-                #with open("results/summary.json", "w") as outfile:
-                #    json.dump(summary, outfile)
 
         except Exception as e:
             st.error(f"An error occurred: {e}")
